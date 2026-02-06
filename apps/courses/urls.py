ae4b6546a5fd86595c09f5cd4b6193727c3db855
Daniel Granada <j.daniel_granada@hotmail.com>
2026-02-06 15:28:11 -0500

    feat(courses): implement CourseListView using CBV with N+1 optimization

---
+++ apps/courses/urls.py
@@ -0,0 +1,9 @@
+# Path: apps/courses/urls.py
+from django.urls import path
+from .views import CourseListView
+
+app_name = 'courses'
+
+urlpatterns = [
+    path('', CourseListView.as_view(), name='course_list'),
+]
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# Path: apps/courses/views.py
+from django.views.generic import ListView
+from .models import Course
 
-# Create your views here.
+# View to list all published courses
+class CourseListView(ListView):
+    model = Course
+    template_name = 'courses/course_list.html'
+    context_object_name = 'courses' 
+    paginate_by = 10 
+
+    def get_queryset(self):
+        return Course.objects.filter(status='published').select_related('teacher')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = "Explore our Courses"
+        return context
