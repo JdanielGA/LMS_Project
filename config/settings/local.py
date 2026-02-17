9599d79648a16a7f7db51f6765e9c5e1cb0095b6
JDanielG <j.daniel_granada@hotmail.com>
2026-02-17 11:12:54 -0500

    chore: install and configure django-debug-toolbar for performance diagnostics

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,4 +1,4 @@
-# Path: ./config/settings/local.py
+# Path: config/settings/local.py
 
 from .base import *
 
@@ -8,3 +8,17 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'noreply@evolaris.academy'
+
+INSTALLED_APPS += [
+    "debug_toolbar",
+]
+
+MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+import socket
+hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+INTERNAL_IPS += [ip[:-1] + "1" for ip in ips]
